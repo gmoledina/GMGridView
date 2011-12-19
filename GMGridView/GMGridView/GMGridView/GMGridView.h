13d1e61f5052488c354dc765df07d540b6b8a178
Peter Steinberger <me@petersteinberger.com>
2011-12-19 18:43:43 +0100

    refactors directory structure.
    Now we have the folder "GMGridView" with the grid classes + an xcodeproj to build.
    Next, there is "Example" which uses the new GMGridView.xcodeproj to link against the project.
    This matches style with most other popular open source projects, and makes it easier to see what's the actual source code and what's the example. This was too mixed up previously.

---
+++ GMGridView/GMGridView/GMGridView/GMGridView.h
@@ -0,0 +1,13 @@
+//
+//  GMGridView.h
+//  GMGridView
+//
+//  Created by Peter Steinberger on 12/19/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GMGridView : NSObject
+
+@end
