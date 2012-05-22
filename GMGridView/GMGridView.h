3d9d851575faf802d891d469449b26ede77cf446
Johnnie Walker <mrwalker@randomsequence.com>
2012-05-22 15:51:30 +0100

    Implements -[GMGridView moveObjectAtIndex:toIndex:] for programmatically rearranging the grid based on changes to the model

--- GMGridView/GMGridView.h
+++ GMGridView/GMGridView.h
@@ -102,6 +102,7 @@ typedef enum
 - (void)insertObjectAtIndex:(NSInteger)index withAnimation:(GMGridViewItemAnimation)animation;
 - (void)removeObjectAtIndex:(NSInteger)index animated:(BOOL)animated;
 - (void)removeObjectAtIndex:(NSInteger)index withAnimation:(GMGridViewItemAnimation)animation;
+- (void)moveObjectAtIndex:(NSInteger)index1 toIndex:(NSInteger)index2;
 - (void)reloadObjectAtIndex:(NSInteger)index animated:(BOOL)animated;
 - (void)reloadObjectAtIndex:(NSInteger)index withAnimation:(GMGridViewItemAnimation)animation;
 - (void)swapObjectAtIndex:(NSInteger)index1 withObjectAtIndex:(NSInteger)index2 animated:(BOOL)animated;
